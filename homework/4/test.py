8487b8266503fe253d9461e8a0f182dffa75c1c4
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2018-07-03 19:20:00 +0300

    Status updated and Tests output with count objects argument

--- homework/4/test.py
+++ homework/4/test.py
@@ -1,7 +1,7 @@
 from encode import *
 from decode import *
 
-def run_test(data, code_type):
+def run_test(data, code_type, count_results):
     result = {'True':[], 'False':[]}
 
     if code_type == 'encode':
@@ -28,6 +28,16 @@ def run_test(data, code_type):
                 lst.append(pair)
                 result['False'] = lst
 
+    if count_results == True:
+        count_true = result['True']
+        count_true = len(count_true)
+        result['True'] = count_true
+        count_false = result['False']
+        count_false = len(count_false)
+        result['False'] = count_false
+
+        return [code_type, result]
+
     return [code_type, result]
 
 
@@ -39,12 +49,15 @@ if __name__ == '__main__':
                     ["WWWWWWWWWWWWB", "12W-1B"],
                     ["ABCD", "-4ABCD"],
                     ["DDDD", "4D"],
-                    ["BAAAABBBCCDCAAM", "-1B4A3B2C-2DC2A-1M"]
+                    ["BAAAABBBCCDCAAM", "-1B4A3B2C-2DC2A-1M"],
+                    ["ABCABCABCABC","-12ABCABCABCABC"],
+                    ["FFFABCABCABCABC","3F-12ABCABCABCABC"]
                 ]
                 
-    result = run_test(test_data, 'encode')
+    result = run_test(test_data, 'encode', count_results=True)
     print(result)
 
-    result = run_test(test_data, 'decode')
+    result = run_test(test_data, 'decode', count_results=True)
     print(result)
+
 
