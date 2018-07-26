d5e01cd32bda1b214be3d98798c9d5221732bfc8
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2018-07-26 16:55:50 +0300

    использовать только методы put и pop

--- homework/5/queue.py
+++ homework/5/queue.py
@@ -36,10 +36,17 @@ class StackAsQueue(object):
         self.stack_1.put(elem)
 
     def dequeue(self):
-        self.stack_2.stack = self.stack_1.stack[1:]
-        result = self.stack_1.stack[0]
-        self.stack_1.stack = self.stack_2.stack
-        self.stack_2 = Stack()
+        
+        for elem in range(len(self.stack_1.stack)):
+            a = self.stack_1.pop()
+            self.stack_2.put(a)
+
+        result = self.stack_2.pop()
+
+        for elem in range(len(self.stack_2.stack)):
+            a = self.stack_2.pop()
+            self.stack_1.put(a)
+
         return result
 
 a = StackAsQueue()
@@ -51,3 +58,15 @@ print(a.stack_1.stack)
 print(a.dequeue())
 print(a.stack_1.stack)
 print(a.stack_2.stack)
+
+'''
+Кирилл Игнатьев, [25.07.18 20:46]
+Нет, так нельзя делать:
+self.stack_1.stack[0]
+Ты используешь stack как массив.
+Представь, что stack_1.stack это приватный член класса, он не входит в интерфейс.
+можешь даже переименовать его в __stack, чтобы не было соблазна.
+
+Можно использовать только методы put и pop, максимум для удобства еще можешь реализовать len()
+'''
+
