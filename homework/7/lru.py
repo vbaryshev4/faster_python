a833ee6d4f4e778eeb4d9baf8d93f57cc9c948ad
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2018-08-07 13:24:24 +0300

    hw07 first version

---
+++ homework/7/lru.py
@@ -0,0 +1,28 @@
+class LRUCache:
+
+	"""docstring for LRUCache"""
+
+	def __init__(self, n):
+		self.cache_size = n
+		self.keys_log = []
+		self.cache = {}
+	
+	def set(self, key, value):
+
+		# добавляет значение value по ключу key. 
+		# Общий размер кэша не должен превысить n. 
+		# Для этого нужно удалять наиболее давно использованные ключи, 
+		# когда кэш достигнет размера n.
+		
+		if len(self.cache) == self.cache_size:
+			key_to_delete = self.keys_log.pop(0)
+			self.cache.pop(key_to_delete)
+
+		self.cache.update({key:value})
+		self.keys_log.append(key)
+		
+	def get(self, key):
+
+		# возвращает value, соответствующее данному ключу.
+
+		return self.cache.get(key)
