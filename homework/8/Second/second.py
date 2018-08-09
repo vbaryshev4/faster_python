0ef401e8d0c8ca83f9293a4aaa54a91773d1b24c
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2018-08-09 16:06:07 +0300

    tests

--- homework/8/Second/second.py
+++ homework/8/Second/second.py
@@ -1,24 +1,3 @@
-'''
-    Есть матрица 2n-1 x 2n-1, заполненная случайными значениями.
-    Надо вывести их на экран в ряд, начиная из центра по спирали: 
-    влево - вниз - вправо - вверх и т.д.
-
-    Если матрица:
-     1  2  3  4  5
-     6  7  8  9 10
-    11 12 13 14 15
-    16 17 18 19 20
-    21 22 23 24 25
-
-    1 2 3
-    4 5 6
-    7 8 9
-
-    То результат:
-    5 4 7 8 9 6 3 2 1
-
-    Решение должно быть для общего случая с любым n.
-'''
 
 class Spiral(object):
     """docstring for Spiral"""
@@ -29,14 +8,15 @@ class Spiral(object):
         self.columns = len(matrix[0])
         self.center = [self.rows//2, self.columns//2]
         self.head = self.center
-        self.result = []
-
+        # self.result = []
 
     def unspiral_matrix(self):
 
+        result = []
+
         def get_value(self):
-            r = self.matrix[self.head[0]][self.head[1]]
-            self.result.append(r)
+            item = self.matrix[self.head[0]][self.head[1]]
+            result.append(item)
 
         def left(self):
             self.head[1] = self.head[1] - 1
@@ -53,7 +33,6 @@ class Spiral(object):
         count = 1
         
         get_value(self)
-        print(self.head, self.result)
         
         while self.head != [0, 0]:
             
@@ -62,12 +41,10 @@ class Spiral(object):
                 for i in range(count):
                     right(self)
                     get_value(self)
-                    print(self.head, self.result)
 
                 for i in range(count):
                     up(self)
                     get_value(self)
-                    print(self.head, self.result)
 
             elif count % 2 == 1:
                 
@@ -75,20 +52,12 @@ class Spiral(object):
                     if self.head != [0, 0]:
                         left(self)
                         get_value(self)
-                        print(self.head, self.result)
 
                 for i in range(count):
                     if self.head != [0, 0]:
                         down(self)
                         get_value(self)
-                        print(self.head, self.result)
 
             count += 1
 
-
-
-
-
-
-
-        
+        return result
--- homework/8/Second/test.py
+++ homework/8/Second/test.py
@@ -1,20 +1,57 @@
 from second import Spiral
-'''
-    Если матрица:
-    1 2 3
-    4 5 6
-    7 8 9
-
-    То результат:
-    5 4 7 8 9 6 3 2 1
-'''
 
 if __name__ == '__main__':
+    cases = {
+                1:{
+                    'matrix':[
+                                [1,2,3],
+                                [4,5,6],
+                                [7,8,9]
+                            ],
+                    'result':[5,4,7,8,9,6,3,2,1]
+                },
+                2:{
+                    'matrix':[
+                                [1,2,3,4,5],
+                                [6,7,8,9,10],
+                                [11,12,13,14,15],
+                                [16,17,18,19,20],
+                                [21,22,23,24,25]
+                            ],
+                    'result':[13,12,17,18,19,14,9,8,7,6,11,16,21,22,23,24,25,20,15,10,5,4,3,2,1]
+                },
+                3:{
+                    'matrix':[
+                                [1,1,1],
+                                [1,1,1],
+                                [1,1,1]
+                            ],
+                    'result':[1,1,1,1,1,1,1,1,1]
+                },
+                4:{
+                    'matrix':[
+                                [-1,-1,-1],
+                                [-1,-1,-1],
+                                [-1,-1,-1]
+                            ],
+                    'result':[-1,-1,-1,-1,-1,-1,-1,-1,-1]
+                },
+                5:{
+                    'matrix':[
+                                ['i','h','g'],
+                                ['b','a','f'],
+                                ['c','d','e']
+                            ],
+                    'result':['a','b','c','d','e','f','g','h','i']
+                },
+            }
     
-    matrix = [[1,2,3],[4,5,6],[7,8,9]]
-    result = Spiral(matrix)
-    # center = result.center
-    # print(matrix[center[0]][center[1]])
-    # print(result.head)
-    print(result.unspiral_matrix())
-    print(result.result)
+    for i in range(1,6):
+        matrix = cases[i]['matrix']
+        item = Spiral(matrix)
+        result = item.unspiral_matrix()
+        if result == cases[i]['result']:
+            print('OK')
+        else:
+            print('False')
+
