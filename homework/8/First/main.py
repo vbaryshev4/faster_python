e2a10d5d91a4e876cad615196c07251118c1c028
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2018-09-04 19:19:49 +0300

    Queue implemented

--- homework/8/First/main.py
+++ homework/8/First/main.py
@@ -4,7 +4,7 @@ import string
 import random
 import redis
 
-message_speed = 1 # in seconds
+message_speed = 1  # in seconds
 message_length = 4
 channels = {
             'messages': 'messages-ch',
@@ -16,23 +16,31 @@ class Behavior:
 
     def __init__(self, status):
         self.status = status
+        self.r = redis.StrictRedis(host='localhost', port=6379, db=1)
+        self.queue = 'message_list'
+
+    def __decode(self, s):
+        return s.decode('utf-8')
+
+    def __push(self, key):
+        r = self.r
+        return r.rpush(self.queue, key)
 
     def produce(self):
-        r = redis.StrictRedis(host='localhost', port=6379, db=1)
+        r = self.r
         while True:
             time.sleep(message_speed)
             s = [random.choices(string.ascii_uppercase)[0] for i in range(message_length)]
             s = ''.join(s)
-            print(r.publish(channels['messages'], s), s)
+            r.set(s, 'published')
+            self.__push(s)
 
     def consume(self):
-        r = redis.StrictRedis(host='localhost', port=6379, db=1)
-        p = r.pubsub()
-        p.subscribe(channels['messages'])
+        r = self.r
+        queue = self.queue
         while True:
-            time.sleep(message_speed)
-            m = p.get_message()
-            print(m)
+            while (r.llen(queue) != 0):
+                print(r.lpop(queue))
 
     def cadidate_rally(self):
         ...
@@ -54,17 +62,3 @@ if __name__ == '__main__':
     app.consume()
 
 
-'''
-    Особенно нам интересен CAS
-    В Питоне примерно так:
-    
-    def client_side_incr(pipe):
-    ...     current_value = pipe.get('OUR-SEQUENCE-KEY')
-    ...     next_value = int(current_value) + 1
-    ...     pipe.multi()
-    ...     pipe.set('OUR-SEQUENCE-KEY', next_value)
-    
-    r.transaction(client_side_incr, 'OUR-SEQUENCE-KEY')
-    
-    (это из документации redis-py: https://pypi.org/project/redis/ )
-'''
