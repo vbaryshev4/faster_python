c1ef94cdb4f89d3b70518bc9a6cc29af5bef569f
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2018-10-09 22:12:48 +0300

    Provider updates

--- homework/11/Provider/importer.py
+++ homework/11/Provider/importer.py
@@ -2,14 +2,46 @@ import psycopg2
 from provider import Provider
 
 """
-    2.1)Importer
-        - Получает данные по курсам валют и проверяет валидность данных (Check Integrity)
-        - Записывает или обновляет данные в базе(Transaction)
-        - Дополняет граф валют до полного(Import Algorithm)
+    Importer
+    - Получает данные по курсам валют и проверяет валидность данных (Check Integrity)
+    - Записывает или обновляет данные в базе(Transaction)
+    - Дополняет граф валют до полного(Import Algorithm)
 """
 
 
+class ControlDb:
+    def __init__(self):
+        self.template =  """insert into rate
+                            values{vals}
+                            ON CONFLICT (cur1, cur2)
+                            DO UPDATE SET
+                            uploadtime = EXCLUDED.uploadtime,
+                            rate = EXCLUDED.rate;"""
+
+    def create_request(self, data):
+        v = (["('{}', '{}', {}, to_timestamp({}))".format(*i) for i in data])
+        v = ','.join(v)
+        return self.template.format(vals=v)
+
 
 if __name__ == '__main__':
+    '''
+        Команды по управлению сервером
+        pg_ctl -D /usr/local/var/postgres start
+        pg_ctl -D /usr/local/var/postgres status
+        Интерактивная консоль базы: psql postgres
+        pg_ctl -D /usr/local/var/postgres stop
+
+    '''
     p = Provider()
-    conn = psycopg2.connect("dbname=suppliers user=postgres password=postgres")
+    data = p.get_data()
+    conn = psycopg2.connect("dbname=postgres user=vbaryshev")
+    cur = conn.cursor()
+    e = ControlDb()
+    r = e.create_request(data)
+    cur.execute(r)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
--- homework/11/Provider/provider.py
+++ homework/11/Provider/provider.py
@@ -1,20 +1,14 @@
-from test import fixer_response_example
+from test import fixer_response, currencylayer_response
+from credentials import creds
 
 '''
-    1) Data Provider(http://data.fixer.io)
-        - Передает данные по курсам валют
-        - Получает данные
+    - Передает данные по курсам валют
+    - Получает данные
 '''
-'''
-    pg_ctl -D /usr/local/var/postgres start
-    pg_ctl -D /usr/local/var/postgres status
-    psql postgres ...
-    pg_ctl -D /usr/local/var/postgres stop
-    
-'''
+
 class Provider:
     def __init__(self):
-        self.sources = [Fixer]
+        self.sources = [Fixer, CurrencyLayer]
 
     def get_data(self):
         for i in self.sources:
@@ -23,16 +17,15 @@ class Provider:
                 raw = test.get_test_value()
                 data = test.get_db_values(raw)
                 return data
-            except ValueError:
+            except:
                 continue
-
 
 class Fixer:
     def __init__(self):
-        self.key = "97ee15850fb7fd028cf83cf807cba6e3"
-        self.url = "http://data.fixer.io/api/"
-        self.query = "http://data.fixer.io/api/latest&base=EUR?access_key=97ee15850fb7fd028cf83cf807cba6e3"
-        self.test_query = fixer_response_example
+        self.key = creds['Fixer']['key']
+        self.url = creds['Fixer']['url']
+        self.query = creds['Fixer']['query']
+        self.test_query = fixer_response
 
     def get_test_value(self):
         return self.test_query
@@ -40,9 +33,25 @@ class Fixer:
     def get_db_values(self, data_set):
         curs = ["RUB", "USD", "EUR", "GBP"]
         base = data_set['base']
-        date = data_set['date']
+        date = data_set['timestamp']
         return [[base, i, data_set['rates'][i], date] for i in data_set['rates'] if i in curs]
 
+
+class CurrencyLayer:
+    def __init__(self):
+        self.key = creds['Currencylayer']['key']
+        self.url = creds['Currencylayer']['url']
+        self.query = creds['Currencylayer']['query']
+        self.test_query = currencylayer_response
+
+    def get_test_value(self):
+        return self.test_query
+
+    def get_db_values(self, data_set):
+        curs = ["RUB", "USD", "EUR", "GBP"]
+        source = data_set['source']
+        date = data_set['timestamp']
+        return [[source, i[3:], data_set['quotes'][i], date] for i in data_set['quotes'] if i[3:] in curs]
 
 
 if __name__ == '__main__':
