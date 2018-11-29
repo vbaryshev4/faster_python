19c409e40064ad15cbed1e7ccdb677310f3274ba
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2018-11-29 18:10:24 +0300

    HW15 with multiprocessing

--- homework/15/pi.py
+++ homework/15/pi.py
@@ -1,16 +1,22 @@
-from random import uniform
+from random import random
+from multiprocessing import Pool
 
-def calc_PI():
-    n_points = 1000000
-    hits = 0
 
-    for i in range(1, n_points):
-        x, y = uniform(0.0, 1.0), uniform(0.0, 1.0)
+def count_hits(n_points):
+    result = 0
+    for i in range(n_points):
+        x, y = random(), random()
+        if (x ** 2 + y ** 2) <= 1:
+            result += 1
+    return result
 
-        if (x**2 + y**2) <= 1.0:
-            hits += 1
 
-    print("Calc: PI result", 4.0 * float(hits) / n_points)
+def calc_PI(accuracy=1):
+    n_points = (10**2)**accuracy
+
+    with Pool(4) as p:
+        hits = (sum(p.map(count_hits, [int(n_points / 4)] * 4)))
+    print("Calc: PI result", round(((4 * hits) / n_points), accuracy))
 
 if __name__ == "__main__":
-    calc_PI()
+    calc_PI(4)
