e3dfed0afeca3bbffa842a0c658bda07973fda63
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2018-12-07 19:31:33 +0300

    HW17 updated

--- homework/17/dsu.py
+++ homework/17/dsu.py
@@ -7,15 +7,19 @@ class DisjointSets:
         self.parents = [i for i in range(n+1)]
 
     def find(self, x):
+        indexes = []
         while x != self.parents[x]:
+            indexes.append(x)
             x = self.parents[x]
-        return x
+        indexes.append(x)
+        # return indexes
+        return [x]
 
     def union(self, x, y):
         root_x = self.find(x)
         root_y = self.find(y)
-        self.parents[root_y] = root_x
-
+        for i in root_y:
+            self.parents[i] = root_x[0]
 
 if __name__ == "__main__":
     for i in cases:
@@ -26,10 +30,12 @@ if __name__ == "__main__":
         print(p.parents)
         for e in input[1]:
             p.union(e[0], e[1])
+            print('e', e)
+            print(p.parents)
         print(p.parents)
         for d in input[2]:
-            root_x = p.find(d[0])
-            root_y = p.find(d[1])
+            root_x = p.find(d[0])[-1]
+            root_y = p.find(d[1])[-1]
             if root_x == root_y:
                 result = 0
-        print(result == expected)
+        print('Test passed:', result == expected, '\n')
