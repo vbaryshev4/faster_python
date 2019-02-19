c4e78c5dabaa256bce098ec606ecbe2fcb0a32b7
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2019-02-19 20:01:46 +0300

    BigO optimization from O(N+M) to O(1)

--- homework/25/main.py
+++ homework/25/main.py
@@ -3,21 +3,20 @@ class ActiveJobs:
         self.log = log
 
     def is_active(self, job_id):
-        for k in self.log.keys():
-            if job_id < k:
-                batch_log = self.log[k]
-                if batch_log['info_by'] == 'open':
-                    return job_id in batch_log['vals']
-                else:
-                    return job_id not in batch_log['vals']
+        k = round(job_id, -3)
+        batch_log = self.log[k]
+        if batch_log['info_by'] == 'open':
+            return job_id in batch_log['vals']
+        else:
+            return job_id not in batch_log['vals']
 
 
 
 if __name__ == '__main__':
     batches = {
-        1000: {'info_by': 'open', 'last_val': 999, 'vals': [10, 555]},
-        2000: {'info_by': 'open', 'last_val': 1999, 'vals': [1900, 1921]},
-        3000: {'info_by': 'closed', 'last_val': 2701, 'vals': [2551, 2602, 2701]}
+        1000: {'info_by': 'open', 'last_val': 999, 'vals': {10, 555}},
+        2000: {'info_by': 'open', 'last_val': 1999, 'vals': {1900, 1921}},
+        3000: {'info_by': 'closed', 'last_val': 2701, 'vals': {2551, 2602, 2701}}
     }
 
     test_cases = [(999, False), (1900, True), (2700, True)]
